a36ec48b73b95aea0a831fafa2bdef8f97966923
akash-kumar-s0 <akashkumars@presidio.com>
2025-04-22 17:28:58 +0530

    basic operations

--- BasicPlayground.playground/Contents.swift
+++ BasicPlayground.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
+import Foundation
 
 var greeting = "Hello, playground"
+print(greeting)
+
+var x: Int
+x=10
+x += 1
+print(x)
+
+// if condion
+
+if x == 11 {
+    print("greater than")
+} else {
+    print( "less than")
+}
+
+
+// guard
+func greet(_ name: String?) {
+    guard let name = name, !name.isEmpty else {
+        print("No name provided.")
+        return
+    }
+    print("Hello, \(name)!")
+}
+greet("")
+greet("akash")
